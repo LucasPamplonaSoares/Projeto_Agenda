fdfd69eb8e82d23cbe20d13de684edc8504bdb7b
Lucas Pamplona Soares <69314618+LucasPamplonaSoares@users.noreply.github.com>
2022-03-14 23:19:04 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Evento
 
-# Register your models here.
+
+class Evento_admin(admin.ModelAdmin):
+    list_display = ('id','titulo', 'data_evento', 'data_criacao') # Quais os campos que eu quero que apareça na listagem
+    list_filter = ('titulo', 'data_evento',) # Assim cria um filtro no site
+
+admin.site.register(Evento, Evento_admin)
